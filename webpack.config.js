438727df5c81dad78b7cb0c820790494bae0c5c8
marcocom <marcocom@gmail.com>
2017-02-16 13:04:26 -0800

    changed dependency and webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,236 @@
 // http://webpack.github.io/docs/configuration.html
 // http://webpack.github.io/docs/webpack-dev-server.html
-var app_root = 'app'; // the app root folder: src, src_users, etc
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const app_root = 'app'; // the app root folder: src, src_users, etc
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack');
+const path = require('path');
+
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const jsSourcePath = path.join(__dirname, './source/js');
+const buildPath = path.join(__dirname, './build');
+const imgPath = path.join(__dirname, './source/assets/img');
+const sourcePath = path.join(__dirname, './source');
+
+if (isProduction) {
+  // Production plugins
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new ExtractTextPlugin('style-[hash].css')
+  );
+
+  // Production rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: 'css-loader!postcss-loader!sass-loader',
+      }),
+    }
+  );
+} else {
+  // Development plugins
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new DashboardPlugin()
+  );
+
+  // Development rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader',
+        // Using source maps breaks urls in the CSS loader
+        // https://github.com/webpack/css-loader/issues/232
+        // This comment solves it, but breaks testing from a local network
+        // https://github.com/webpack/css-loader/issues/232#issuecomment-240449998
+        // 'css-loader?sourceMap',
+        'css-loader',
+        'postcss-loader',
+        'sass-loader?sourceMap',
+      ],
+    }
+  );
+}
 
 module.exports = {
   app_root, // the app root folder, needed by the other webpack configs
+  devtool: isProduction ? 'eval' : 'source-map', //was inline-source-map
+  context: jsSourcePath,
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     'babel-polyfill',
-    __dirname + '/' + app_root + '/index.js',
+    (__dirname + '/') + (app_root + '/index.js'),
   ],
-  devtool: 'inline-source-map',
+  entry: {
+    js: './index.js',
+    vendor: [
+      'babel-polyfill',
+      'es6-promise',
+      'immutable',
+      'isomorphic-fetch',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react',
+      'redux-thunk',
+      'redux',
+    ],
+  },
   output: {
-    path: __dirname + '/dist',
+    path: buildPath,
     publicPath: '/',
     filename: 'app-[hash].js',
   },
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loaders: ['babel', 'eslint-loader'],
+        test: /\(.js|.jsx)$/,
+        loaders: ['babel-loader', 'eslint-loader'],
         exclude: /node_modules/,
       },
       {
         test: /\.less$/,
-        loader: 'style!css!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap',
+        loader: 'style-loader!css!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap',
         exclude: /node_modules/,
       },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'raw-loader' },
-      { test: /([^\s]+(\.(jpg|png|gif|bmp))$)/, loader: 'url-loader?limit=10240' }
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          mimetype: 'application/font-woff'
+        }
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          mimetype: 'application/font-woff'
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          mimetype: 'application/octet-stream'
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /([^\s]+(\.(jpg|png|gif|bmp))$)/,
+        loader: 'url-loader' ,
+        query: {
+          limit:10240,
+          name:'assets/[name]-[hash].[ext]'
+        }
+      }
     ],
   },
   progress: true,
   resolve: {
-    modulesDirectories: [
-      app_root,
-      'node_modules'
-    ],
-    extensions: ['', '.json', '.js', '.jsx']
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.json', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      jsSourcePath,
+    ]
   },
   devServer: {
-    contentBase: __dirname + '/dist',
+    contentBase: isProduction ? './build' : './source',
     historyApiFallback: true,
+    port: 3000,
+    compress: isProduction,
+    inline: !isProduction,
+    hot: !isProduction,
+    host: '0.0.0.0',
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      },
+    },
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor-[hash].js',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(sourcePath, 'index.html'),
+      path: buildPath,
+      filename: 'index.html',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: [
+              'last 3 version',
+              'ie >= 10',
+            ],
+          }),
+        ],
+        context: sourcePath,
+      },
+    }),
+  
+    /* olde
     new CleanWebpackPlugin(['*'], {
       root: __dirname + '/dist',
       verbose: true,
@@ -62,6 +240,7 @@ module.exports = {
       template: __dirname + '/' + app_root + '/index.html',
       filename: 'index.html',
       inject: 'body',
-    })
+    }),
+    */
   ],
 };
